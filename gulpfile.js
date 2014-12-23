fdb2982a6fda0ca7d85e188a354dad402412053e
Hovhannes Babayan <hovhannesbabayan@attask.com>
2014-12-23 14:19:11 +0400

    added gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,47 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var del = require('del');
+var mocha = require('gulp-mocha');
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
-	// place code for your default task here
+const BUILD_DIR = './dist/';
+
+
+gulp.task('default', ['clean', 'build']);
+
+/**
+ * Empties BUILD_DIR
+ */
+gulp.task('clean', function(cb) {
+	del([BUILD_DIR + '*'], cb);
 });
 
+/**
+ * Generates browser-ready version for API in BUILD_DIR
+ * File will be named as api.js
+ */
+gulp.task('build', function() {
+	return browserify('./src/api.js')
+		.bundle()
+		.pipe(source('api.js'))
+		.pipe(gulp.dest(BUILD_DIR));
+});
+
+
+gulp.task('serve', function() {
+	gulp.src('./')
+		.pipe(webserver({
+			hostname: 'localhost',
+			port: 8000,
+			livereload: true,
+			directoryListing: true,
+			open: true
+		}));
+});
+
+
 gulp.task('test', function() {
-	console.log('test')
+	return gulp.src('test/**/*Spec.js', {read: false})
+		.pipe(mocha({}));
 });
