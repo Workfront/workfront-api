945d0da3edde7501a5ea721dcec14a61c514478f
Hovhannes Babayan <hovhannesbabayan@attask.com>
2014-12-26 17:42:47 +0400

    added example to get users count

---
+++ examples/node/get-user-count.js
@@ -0,0 +1,38 @@
+/**
+ * Logs in, returns number of active users
+ */
+
+var ApiFactory = require('./../../').ApiFactory;
+var ApiConstants = require('./../../').ApiConstants;
+var util = require('util');
+
+var instance = ApiFactory.getInstance({
+	hostname: 'localhost',
+	version: '5.0',
+	port: 8080
+});
+
+util.print('Logs in, returns number of active users\n');
+util.log('Logging in ...');
+instance.login('new@user.attask', 'user').then(
+	function() {
+		util.log('Getting number of active users ...');
+		var query = {};
+		query['isActive'] = true;
+		query['isActive' + ApiConstants.MOD] = ApiConstants.Operators.EQUAL;
+		instance.count('user', query).then(
+			function(data) {
+				util.log('Get success. Received data:');
+				console.log(util.inspect(data, {colors:true}));
+			},
+			function(error) {
+				util.log('Get failure. Received data:');
+				console.log(util.inspect(error, {colors:true}));
+			}
+		);
+	},
+	function(error) {
+		util.log('Login failure. Received data:');
+		console.log(util.inspect(error, {colors:true}));
+	}
+);
