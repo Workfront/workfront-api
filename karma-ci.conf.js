ebe3532de7ac5a3b586a2070527c5144b130062a
Sassoun Derderian <sassounderderian@workfront.com>
2017-03-14 18:08:01 +0400

    chore: travis.yaml set CI env variable

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -2,63 +2,64 @@
 
 const karmaConfig = require('./karma.conf');
 
-module.exports = function (config) {
-    karmaConfig(config)
+const CI = process.env.CI
 
-    // Browsers to run on Sauce Labs
-    const customLaunchers = {
-        'bs_chrome_mac': {
-            base: 'BrowserStack',
-            browser: 'chrome',
-            browser_version: '56.0',
-            os: 'OS X',
-            os_version: 'Sierra'
-        },
-        'bs_firefox_mac': {
-            base: 'BrowserStack',
-            browser: 'firefox',
-            browser_version: '52.0',
-            os: 'OS X',
-            os_version: 'Sierra'
-        },
-        'bs_internet_explorer_win': {
-            base: 'BrowserStack',
-            browser: 'internet explorer',
-            browser_version: '11.0',
-            os: 'Windows',
-            os_version: '7'
-        },
-        'bs_safari_mac': {
-            base: 'BrowserStack',
-            browser: 'safari',
-            browser_version: '10.0',
-            os: 'OS X',
-            os_version: 'Sierra'
-        }
-    };
+if(CI === 'TRAVIS') {
+    module.exports = karmaConfig
+}
+else if (CI === 'BS') {
+    module.exports = function (config) {
+        karmaConfig(config)
 
-    // Override config for CI.
-    config.set({
-        reporters: ['progress'],
-        // sauceLabs: {
-        //     testName: 'workfront-api CI tests',
-        //     recordScreenshots: false,
-        //     connectOptions: {
-        //         port: 5757
-        //     },
-        //     public: 'public'
-        // },
-        // captureTimeout: 120000,
-        browserStack: {
-            username: process.env.BROWSERSTACK_USER,
-            accessKey: process.env.BROWSERSTACK_ACCESSKEY,
-        },
-        customLaunchers: customLaunchers,
+        // Browsers to run on Sauce Labs
+        const customLaunchers = {
+            'bs_chrome_mac': {
+                base: 'BrowserStack',
+                browser: 'chrome',
+                browser_version: '56.0',
+                os: 'OS X',
+                os_version: 'Sierra'
+            },
+            'bs_firefox_mac': {
+                base: 'BrowserStack',
+                browser: 'firefox',
+                browser_version: '52.0',
+                os: 'OS X',
+                os_version: 'Sierra'
+            },
+            'bs_internet_explorer_win': {
+                base: 'BrowserStack',
+                browser: 'ie',
+                browser_version: '11',
+                os: 'Windows',
+                os_version: '7'
+            },
+            'bs_safari_mac': {
+                base: 'BrowserStack',
+                browser: 'safari',
+                browser_version: '10.0',
+                os: 'OS X',
+                os_version: 'Sierra'
+            }
+        };
 
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: Object.keys(customLaunchers),
-        singleRun: true
-    })
+        // Override config for CI.
+        config.set({
+            reporters: ['progress'],
+            browserStack: {
+                username: process.env.BROWSERSTACK_USER,
+                accessKey: process.env.BROWSERSTACK_ACCESSKEY,
+                project: 'workfront-api'
+            },
+            customLaunchers: customLaunchers,
 
+            // start these browsers
+            // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+            browsers: Object.keys(customLaunchers),
+            singleRun: true
+        })
+    }
+}
+else {
+    console.log('Please specify CI environment variable')
 }
