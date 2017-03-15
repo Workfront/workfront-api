4841f77456939fc39a6fe8f531ed087320a8cc9e
Sassoun Derderian <sassounderderian@workfront.com>
2017-03-15 11:03:53 +0400

    SauceLabs integration

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -13,44 +13,81 @@ else if (CI === 'BS') {
 
         // Browsers to run on Sauce Labs
         const customLaunchers = {
-            'bs_chrome_mac': {
-                base: 'BrowserStack',
-                browser: 'chrome',
-                browser_version: '56.0',
-                os: 'OS X',
-                os_version: 'Sierra'
+        'SL_Chrome': {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            version: '48.0',
+            platform: 'Linux'
             },
-            'bs_firefox_mac': {
-                base: 'BrowserStack',
-                browser: 'firefox',
-                browser_version: '52.0',
-                os: 'OS X',
-                os_version: 'Sierra'
+        'SL_Chrome56': {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            version: '56.0',
+            platform: 'OS X 10.12'
             },
-            'bs_internet_explorer_win': {
-                base: 'BrowserStack',
-                browser: 'ie',
-                browser_version: '11',
-                os: 'Windows',
-                os_version: '7'
+        'SL_Chrome55': {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            version: '55.0',
+            platform: 'OS X 10.12'
             },
-            'bs_safari_mac': {
-                base: 'BrowserStack',
-                browser: 'safari',
-                browser_version: '10.0',
-                os: 'OS X',
-                os_version: 'Sierra'
+        'SL_Firefox50': {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            version: '50.0',
+            platform: 'Windows 10'
+            },
+        'SL_Firefox51': {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            version: '51.0',
+            platform: 'Windows 10'
+            },
+        'SL_InternetExplorer10': {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '11.0',
+            platform: 'Windows 7'
+            },
+        'SL_InternetExplorer11': {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '11.0',
+            platform: 'Windows 7'
+            },
+        'SL_InternetExplorer13': {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '13.0',
+            platform: 'Windows 10'
+            },
+        'SL_Safari10': {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            platform: 'OS X 10.12',
+            version: '10.0'
+            },
+        'SL_Safari9': {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            platform: 'OS X 10.11',
+            version: '9.0'
             }
         };
 
         // Override config for CI.
         config.set({
-            reporters: ['progress'],
-            browserStack: {
-                username: process.env.BROWSERSTACK_USER,
-                accessKey: process.env.BROWSERSTACK_ACCESSKEY,
-                project: 'workfront-api'
+            reporters: ['spec', 'coverage', 'saucelabs'],
+            sauceLabs: {
+                testName: 'workfront-api',
+                recordScreenshots: false,
+                recordVideo: false,
+                connectOptions: {
+                    port: 5757
+                },
+                public: 'public'
             },
+            captureTimeout: 120000,
             customLaunchers: customLaunchers,
 
             // start these browsers
