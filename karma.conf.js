cd688d38bbfbcb25b0b5c400e6560ce97be16140
Sassoun Derderian <sassounderderian@workfront.com>
2017-07-19 12:50:30 +0400

    fix: sauce access_key and username set as env variables

--- karma.conf.js
+++ karma.conf.js
@@ -114,6 +114,7 @@ module.exports = function (config) {
                 public: 'public',
                 recordScreenshots: false,
                 recordVideo: false,
+                startConnect: false,
                 testName: 'workfront-api',
                 tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
             },
