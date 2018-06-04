4c415f479290eb180784e2c11c50702fecca6e1b
Hovhannes Babayan <bhovhannes@gmail.com>
2018-06-04 18:37:57 +0400

    chore: attempt to fix soucelabs errors

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: {
 		'./dist/workfront': './src/Api.ts'
     },
-    mode: 'development',
+    mode: 'production',
     output: {
         path: __dirname,
         filename: '[name].js',
