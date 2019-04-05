3ecc58b43040071c59517f82f741476526d202a1
Sassoun Derderian <citizen.sas@gmail.com>
2019-04-05 13:19:15 +0400

    chore(dev): faster rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -5,34 +5,42 @@ import { terser } from 'rollup-plugin-terser'
 
 const name = 'workfront-api'
 
-const umdOptions = (minify) => ({
-    input: 'src/Api.ts',
-    output: {
-        file: minify ? `dist/${name}.umd.min.js` : `dist/${name}.umd.js`,
-        format: 'umd',
-        name: 'Workfront',
-        sourcemap: true
+module.exports = [
+    {
+        input: 'src/Api.ts',
+        output: [
+            {
+                file: `dist/${name}.es.js`,
+                format: 'esm',
+                sourcemap: true
+            },
+            {
+                file: `dist/${name}.umd.js`,
+                format: 'umd',
+                name: 'Workfront',
+                sourcemap: true
+            },
+        ],
+        plugins: [ typescript(), resolve(), commonjs() ]
     },
-    plugins: [ typescript(), resolve(), commonjs(), minify ? terser() : undefined ],
-})
-const esOptions = () => ({
-    input: 'src/Api.ts',
-    output: {
-        file: `dist/${name}.es.js`,
-        format: 'esm',
-        sourcemap: true
+    {
+        input: 'src/Api.ts',
+        output: {
+            file: `dist/${name}.umd.min.js`,
+            format: 'umd',
+            name: 'Wokfront',
+            sourcemap: true
+        },
+        plugins: [ typescript(), resolve(), commonjs(), terser() ]
     },
-    plugins: [ typescript(), resolve(), commonjs() ],
-})
-const cjsOptions = () => ({
-    input: 'src/node.ts',
-    output: {
-        file: `dist/${name}.cjs.js`,
-        format: 'cjs',
-        sourcemap: true
-    },
-    plugins: [ typescript(), terser() ],
-    external: ['form-data', 'isomorphic-fetch', 'workfront-api-constants']
-})
-
-module.exports = [umdOptions(), esOptions(), umdOptions(true), cjsOptions()]
+    {
+        input: 'src/node.ts',
+        output: {
+            file: `dist/${name}.cjs.js`,
+            format: 'cjs',
+            sourcemap: true
+        },
+        plugins: [ typescript() ],
+        external: ['form-data', 'isomorphic-fetch', 'workfront-api-constants']
+    }
+]
